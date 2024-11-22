6f8ded6d4eb8f00657b4b573e19629b424bf184f
Gideonayodeji <gideonayodeji5@gmail.com>
2024-11-22 20:24:40 +0100

    first commit

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for reaching out! I will get back to you soon.');
-});
